82760dba19780f5e56aee3e0a5518e038affe48f
Sumithra <sumisooriya@gmail.com>
2022-10-26 18:04:45 +0530

    Initial commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+var scaffoldcolor = const Color(0xff030708);
+var primarytextcolor = const Color(0xffFFFFFF);
+var iconbg = const Color(0xff332F2F);
+var greencardbg = const Color(0xff062526);
+var greentext = const Color(0xff41D4AF);
+var errortext = const Color(0xffFF4F4F);
+var yellowtext = const Color(0xffFFCE4F);
+var alertcardcolor = const Color(0xff4E4632);
