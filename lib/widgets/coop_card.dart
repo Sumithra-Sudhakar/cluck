80776f0d5a897af42c0ba1130df4538b1b8b88e0
Sumithra <sumisooriya@gmail.com>
2022-12-07 13:41:25 +0530

    embedded dialogue completed

--- lib/widgets/coop_card.dart
+++ lib/widgets/coop_card.dart
@@ -12,7 +12,6 @@ class CoopCard extends StatefulWidget {
   State<CoopCard> createState() => _CoopCardState();
 }
 
-//TODO: DIALOGUE BOX FOR MODIFYING COOP VALUES
 class _CoopCardState extends State<CoopCard> {
   @override
   Widget build(BuildContext context) {
@@ -47,7 +46,7 @@ class _CoopCardState extends State<CoopCard> {
                         children: [
                           Column(
                             mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start  ,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
                                 "Coop1",
@@ -119,42 +118,250 @@ class _CoopCardState extends State<CoopCard> {
                           children: [
                             Row(
                               children: [
-                                Icon(Icons.thermostat,size: 45,color: colors.primaryTextColor,),
-                                Text('28°',
-                                style: GoogleFonts.raleway(
-                                  fontWeight: FontWeight.w400,
+                                Icon(
+                                  Icons.thermostat,
+                                  size: 45,
                                   color: colors.primaryTextColor,
-                                  fontSize: 40
-                                ),)
+                                ),
+                                Text(
+                                  '28°',
+                                  style: GoogleFonts.raleway(
+                                      fontWeight: FontWeight.w400,
+                                      color: colors.primaryTextColor,
+                                      fontSize: 40),
+                                )
                               ],
                             ),
                             Row(
                               children: [
-                                Icon(Icons.water_drop_outlined,size: 40,color: colors.primaryTextColor,),
-                                Text('80%°',
+                                Icon(
+                                  Icons.water_drop_outlined,
+                                  size: 40,
+                                  color: colors.primaryTextColor,
+                                ),
+                                Text(
+                                  '80%°',
                                   style: GoogleFonts.raleway(
                                       fontWeight: FontWeight.w400,
                                       color: colors.primaryTextColor,
-                                      fontSize: 40
-                                  ),)
+                                      fontSize: 40),
+                                )
                               ],
                             )
                           ],
                         ),
                       ),
                       Padding(
-                        padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.03),
+                        padding: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.03),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.end,
                           children: [
                             TextButton(
                                 onPressed: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            const LoadingScreen(),
-                                      ));
+                                  showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                            contentPadding:
+                                                const EdgeInsets.all(30),
+                                            elevation: 5,
+                                            backgroundColor: Colors.black,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(20.0),
+                                            ),
+                                            content: SizedBox(
+                                                height: MediaQuery.of(context)
+                                                        .size
+                                                        .height *
+                                                    0.6,
+                                                child: Column(
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment.start,
+                                                    children: [
+                                                      Image.asset(
+                                                        "assets/coop.png",
+                                                        height: 200,
+                                                        fit: BoxFit.cover,
+                                                      ),
+                                                      SizedBox(
+                                                        height: 20,
+                                                      ),
+                                                      Row(
+                                                        mainAxisAlignment:
+                                                            MainAxisAlignment
+                                                                .spaceBetween,
+                                                        children: [
+                                                          Column(
+                                                            mainAxisAlignment:
+                                                                MainAxisAlignment
+                                                                    .start,
+                                                            crossAxisAlignment:
+                                                                CrossAxisAlignment
+                                                                    .start,
+                                                            children: [
+                                                              Text(
+                                                                "Coop1",
+                                                                textAlign:
+                                                                    TextAlign
+                                                                        .start,
+                                                                style: GoogleFonts.raleway(
+                                                                    color: colors
+                                                                        .primaryTextColor,
+                                                                    fontSize:
+                                                                        20),
+                                                              ),
+                                                              SizedBox(
+                                                                height: 10,
+                                                              ),
+                                                              Text(
+                                                                "Breed name 1",
+                                                                style: GoogleFonts.raleway(
+                                                                    color: colors
+                                                                        .primaryTextColor,
+                                                                    fontSize:
+                                                                        18),
+                                                              ),
+                                                            ],
+                                                          ),
+                                                          Column(
+                                                            children: [
+                                                              Row(
+                                                                children: [
+                                                                  Icon(
+                                                                    Icons
+                                                                        .animation,
+                                                                    color: colors
+                                                                        .primaryTextColor,
+                                                                  ),
+                                                                  Padding(
+                                                                    padding: EdgeInsets.symmetric(
+                                                                        horizontal:
+                                                                            10),
+                                                                    child: Text(
+                                                                      "122",
+                                                                      style: GoogleFonts.raleway(
+                                                                          color: colors
+                                                                              .primaryTextColor,
+                                                                          fontSize:
+                                                                              15),
+                                                                    ),
+                                                                  ),
+                                                                ],
+                                                              ),
+                                                              SizedBox(
+                                                                height: 10,
+                                                              ),
+                                                              Row(
+                                                                children: [
+                                                                  Icon(
+                                                                    Icons
+                                                                        .emoji_food_beverage,
+                                                                    color: colors
+                                                                        .primaryTextColor,
+                                                                  ),
+                                                                  Padding(
+                                                                    padding: EdgeInsets.symmetric(
+                                                                        horizontal:
+                                                                            10),
+                                                                    child: Text(
+                                                                      "54Kg",
+                                                                      style: GoogleFonts.raleway(
+                                                                          color: colors
+                                                                              .primaryTextColor,
+                                                                          fontSize:
+                                                                              15),
+                                                                    ),
+                                                                  ),
+                                                                ],
+                                                              ),
+                                                            ],
+                                                          )
+                                                        ],
+                                                      ),
+                                                      Padding(
+                                                        padding:
+                                                            const EdgeInsets
+                                                                    .symmetric(
+                                                                vertical: 30.0),
+                                                        child: Row(
+                                                          mainAxisAlignment:
+                                                              MainAxisAlignment
+                                                                  .spaceBetween,
+                                                          children: [
+                                                            Row(
+                                                              children: [
+                                                                Icon(
+                                                                  Icons
+                                                                      .thermostat,
+                                                                  size: 45,
+                                                                  color: colors
+                                                                      .primaryTextColor,
+                                                                ),
+                                                                Text(
+                                                                  '28°',
+                                                                  style: GoogleFonts.raleway(
+                                                                      fontWeight:
+                                                                          FontWeight
+                                                                              .w400,
+                                                                      color: colors
+                                                                          .primaryTextColor,
+                                                                      fontSize:
+                                                                          40),
+                                                                )
+                                                              ],
+                                                            ),
+                                                          ],
+                                                        ),
+                                                      ),
+                                                      SizedBox(
+                                                        height: 30,
+                                                      ),
+                                                      Row(
+                                                        mainAxisAlignment:
+                                                            MainAxisAlignment
+                                                                .end,
+                                                        children: [
+                                                          TextButton(
+                                                              onPressed: () {
+                                                                Navigator.pop(
+                                                                    context);
+                                                              },
+                                                              child: Text(
+                                                                "CANCEL",
+                                                                style:
+                                                                    GoogleFonts
+                                                                        .raleway(
+                                                                  fontSize: 15,
+                                                                  color: colors
+                                                                      .greenText,
+                                                                ),
+                                                              )),
+                                                          TextButton(
+                                                            onPressed: () {
+                                                              Navigator.pop(
+                                                                  context);
+                                                            },
+                                                            style: ButtonStyle(
+                                                                backgroundColor:
+                                                                    MaterialStateProperty
+                                                                        .all(colors
+                                                                            .greenCardBG)),
+                                                            child: Text(
+                                                              "OKAY",
+                                                              style: GoogleFonts
+                                                                  .raleway(
+                                                                fontSize: 15,
+                                                                color: colors
+                                                                    .greenText,
+                                                              ),
+                                                            ),
+                                                          ),
+                                                        ],
+                                                      )
+                                                    ])));
+                                      });
                                 },
                                 child: Text(
                                   "MODIFY",
@@ -165,7 +372,7 @@ class _CoopCardState extends State<CoopCard> {
                                 )),
                             TextButton(
                               onPressed: () {
-
+                                Navigator.pop(context);
                               },
                               style: ButtonStyle(
                                   backgroundColor: MaterialStateProperty.all(
